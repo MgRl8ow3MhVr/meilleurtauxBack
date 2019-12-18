756cab1a408e126cbfd2ad9018dc671e840abb88
MgRl8ow3MhVr <7anUDQ2A3MVJkC7J@gmail.com>
2019-12-18 19:58:26 +0100

    starter pack

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const formidable = require("express-formidable");
+
 const app = express();
+app.use(formidable());
+app.use(cors());
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+});
+
+const User = mongoose.model("User", {
+  name: String,
+  city: String
+});
 
 app.get("/ping", (req, res) => {
   res.send("pong");
+  const user = new User();
+  user.name = "pierre";
+  user.city = "paris";
+  user.save();
 });
 
 app.listen(process.env.PORT, () => {
