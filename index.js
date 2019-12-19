06d0dfbcbc13f8b2aa52b8d856d325a2ef28b9a6
MgRl8ow3MhVr <7anUDQ2A3MVJkC7J@gmail.com>
2019-12-19 17:55:13 +0100

    jeudi soir

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 // # # # # # # # IMPORTS AND DEFINITIONS # # # # # # #
 
+// Imports
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const formidable = require("express-formidable");
+const mailgun = require("mailgun-js");
 
 // Server app declaration
 const app = express();
 app.use(formidable());
 app.use(cors());
 
-// Mongoose connection
+// # # # # # # # MONGOOSE CONNECTION AND MODELS # # # # # # #
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true
 });
 
-// Demande de Devis Model Definition
 const Devis = mongoose.model("Devis", {
   email: String,
   type: String,
@@ -29,14 +30,19 @@ const Devis = mongoose.model("Devis", {
   zip: String
 });
 
+// # # # # # # # MAILGUN SETTINGS # # # # # # #
+const API_KEY = process.env.GUNKEY;
+const DOMAIN = process.env.GUNDOMAIN;
+const mg = mailgun({ apiKey: API_KEY, domain: DOMAIN });
+
 // # # # # # # # ROUTES # # # # # # # # # # # # # #
 
-// - - - - - - - - - - HELLO WORLD - - - - - - - -
+// - - - - - - - - - - HELLO WORLD - - - - - - - - - - - - - - - - - - - - -
 app.get("/", async (req, res) => {
   res.status(200).json({ message: "Meilleur Taux Back te passe le bonjour" });
 });
 
-// - - - - - - - - - - PING - - - - - - - -
+// - - - - - - - - - - PING - - - - - - - - - - - - - - - - - - - - - - - -
 app.get("/ping", async (req, res) => {
   try {
     res.status(200).json({ message: "pong" });
@@ -45,22 +51,52 @@ app.get("/ping", async (req, res) => {
     res.status(400).json({ message: e.message });
   }
 });
-// - - - - - - - - - - DEVIS CREATION - - - - - - - -
+
+// - - - - - - - - - - DEVIS CREATION - - - - - - - - - - - - - - - - - - -
 app.post("/deviscreation", async (req, res) => {
   try {
+    // Create new Entry
+    console.log("devis creation asked", req.fields.email);
     const devis = new Devis(req.fields);
     await devis.save();
-    console.log("creation", devis);
 
-    res.status(200).json({ message: "Created" });
+    // Send an Email
+    console.log("creation OK, starting to send email");
+    const data = {
+      from: "Mailgun Sandbox <postmaster@" + DOMAIN + ">",
+      to: "7anUDQ2A3MVJkC7J@gmail.com",
+      subject: "Meilleur Taux Pierre, Dossier " + devis._id,
+      text:
+        "Voici ton récapitulatif Meilleur Taux en JSON, débrouille toi avec " +
+        JSON.stringify(devis)
+    };
+    mg.messages().send(data, function(error, body) {
+      console.log(body);
+    });
+
+    // Send Back Res Status OK
+    console.log("email done, answering client with id", devis._id);
+    res.status(200).json({ message: "Created", id: devis._id });
   } catch (e) {
     console.log(e.message);
     res.status(400).json({ message: e.message });
   }
 });
 
-// # # # # # # # STARTER # # # # # # # # # # # # # #
+// - - - - - - - - - - GET ALL DEVIS - - - - - - - - - - - - - - - - - - -
 
+app.get("/getdevis", async (req, res) => {
+  try {
+    console.log("get all required");
+    const alldevis = await Devis.find();
+    console.log(alldevis);
+    res.status(200).json({ alldevis: alldevis });
+  } catch (e) {
+    res.status(400).json({ message: e.message });
+  }
+});
+
+// # # # # # # # STARTER # # # # # # # # # # # # # #
 app.listen(process.env.PORT, () => {
   console.log("server started on", process.env.PORT);
 });
